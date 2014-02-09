619cb3478a0ec77a7e23860267f3e33cec49aac9
Hardmath123 <hardmath123@gmail.com>
2014-02-09 14:09:49 -0800

    non-capturing groups and curly brace things

--- regex.js
+++ regex.js
@@ -10,8 +10,12 @@
         var CHARSET = {};
         var CHARSETCHAR = {};
         var CHARSETCHARLIST = {};
-        var yuck = /[^\\?*+)(|\[\]]/
+        var INT = {};
+        var yuck = /[^\\?*+)(|\[\]\{\}]/
         var rules = [
+            new Earley.Rule(INT, [/\d/], function(d) {return d[0];}),
+            new Earley.Rule(INT, [INT, /\d/], function(d) {return d[0]+d[1];}),
+
             new Earley.Rule(CHAR, [yuck], id),
             new Earley.Rule(CHAR, ["\\", /./], function (d) {return d[0]+d[1];}),
 
@@ -27,11 +31,14 @@
             new Earley.Rule(CHARSETCHARLIST, [CHARSETCHARLIST,CHARSETCHAR], function(d){return d[0].concat(d[1])}),
 
             new Earley.Rule(PAREN, ["(", PATTERN, ")"], function(d){return {type:"paren", arg:d[1]};}),
+            new Earley.Rule(PAREN, ["(", "?", ":", PATTERN, ")"], function(d){return {type:"paren", "noncapturing":true, arg:d[3]};}),
             new Earley.Rule(PAREN, [CHARSET], id),
 
             new Earley.Rule(KLEENE, [KLEENE, "*"], function(d) {return {type:"*", arg:d[0]}}),
             new Earley.Rule(KLEENE, [KLEENE, "?"], function(d) {return {type:"?", arg:d[0]}}),
             new Earley.Rule(KLEENE, [KLEENE, "+"], function(d) {return {type:"+", arg:d[0]}}),
+            new Earley.Rule(KLEENE, [KLEENE, "{", INT, "}"], function(d) {return {type:"{1}", arg:d[0], n:d[2]}}),
+            new Earley.Rule(KLEENE, [KLEENE, "{", INT, ",", INT, "}"], function(d) {return {type:"{2}", arg:d[0], ns:d[2]+d[4]}}),
             new Earley.Rule(KLEENE, [PAREN], id),
 
             new Earley.Rule(CONCATENATION, [KLEENE]),
@@ -57,11 +64,17 @@
             if (parsed.type === "|") {
                 return Stringify(parsed.arg[0]) + "|" + parsed.arg[1];
             } else if (parsed.type === "paren") {
-                return "(" + Stringify(parsed.arg) + ")";
+                return "(" + (parsed.noncapturing ? "?:" : "") + Stringify(parsed.arg) + ")";
             } else if (parsed.type === "charset") {
                 return "["+(parsed.negated ? "^" : "")+Stringify(parsed.data)+"]";
             } else if (parsed.type === "charsetrange") {
                 return parsed.from+"-"+parsed.to;
+            } else if (parsed.type === "{1}") {
+                //return parsed.from+"-"+parsed.to;
+                return parsed.arg + "{" + parsed.n + "}";
+            } else if (parsed.type === "{2}") {
+                //return parsed.from+"-"+parsed.to;
+                return parsed.arg + "{" + parsed.ns[0] + "," + parsed.ns[1] + "}";
             } else {
                 return Stringify(parsed.arg[0]) + parsed.type;
             }
@@ -94,7 +107,7 @@
                             "\\B": "Something that isn't a boundary between words",
                             "$": "End of the input",
                             "^": "The beginning of the input",
-                        }[parsed] || "A '"+parsed.charAt(1)+"'")
+                        }[parsed] ||  (/\d/.test(parsed) && "The result of the "+parsed.charAt(1)+"th capturing group.")  || "A '"+parsed.charAt(1)+"'")
                     );
                 }, false);
                 out.addEventListener("mouseout", function(){
@@ -142,7 +155,7 @@
             } else if (parsed.type === "paren") {
                 var out = document.createElement("span");
                 var lp = document.createElement("span");
-                lp.innerHTML = "(";
+                lp.innerHTML = parsed.noncapturing ? "(?:" : "(";
                 var rp = document.createElement("span");
                 rp.innerHTML = ")";
                 lp.className = "paren";
@@ -153,10 +166,16 @@
                 lp.addEventListener("mouseover", function(){
                     lp.className += " highlit";
                     rp.className += " highlit";
+                    if (parsed.noncapturing) {
+                        document.getElementById("output-explanation").appendChild(document.createTextNode("Non-capturing group."));
+                    }
                 }, false);
                 lp.addEventListener("mouseout", function(){
                     lp.className = lp.className.replace("highlit", "");
                     rp.className = lp.className.replace("highlit", "");
+                    if (parsed.noncapturing) {
+                        document.getElementById("output-explanation").removeChild(document.getElementById("output-explanation").firstChild);
+                    }
                 }, false);
                 rp.addEventListener("mouseover", function(){
                     lp.className += " highlit";
@@ -172,15 +191,12 @@
 
 
                 var lp = document.createElement("span");
-                lp.innerHTML = "[";
+                lp.innerHTML = parsed.negated ? "[^" : "[";
                 var rp = document.createElement("span");
                 rp.innerHTML = "]";
                 lp.className = "paren";
                 rp.className = "paren";
                 out.appendChild(lp);
-                if (parsed.negated) {
-                    out.appendChild(document.createTextNode("^"));
-                }
                 out.appendChild(Highlight(parsed.data));
                 out.appendChild(rp);
                 lp.addEventListener("mouseover", function(){
@@ -234,7 +250,8 @@
                 var arg = Highlight(parsed.arg);
                 var text = document.createElement("span");
                 text.className = "symbol";
-                text.innerHTML = parsed.type;
+                text.innerHTML = parsed.type === "{1}" ? "{"+parsed.n+"}" :
+                                parsed.type === "{2}" ? "{"+parsed.ns[0]+","+parsed.ns[1]+"}" : parsed.type;
                 out.appendChild(arg);
                 out.appendChild(text);
                 text.addEventListener("mouseover", function(){
@@ -244,7 +261,9 @@
                         document.createTextNode({
                         "*": "Match an arbitrary number of '" + Stringify(parsed.arg) + "'s",
                         "+": "Match at least one '" + Stringify(parsed.arg)+"'",
-                        "?": "A '" + Stringify(parsed.arg) + "' may or may not exist."
+                        "?": "A '" + Stringify(parsed.arg) + "' may or may not exist.",
+                        "{1}": "Match " + parsed.n + " of " + Stringify(parsed.arg),
+                        "{2}": "Match anywhere from " + (parsed.ns&&parsed.ns[0]) + " to " + (parsed.ns&&parsed.ns[1]) + " of " + Stringify(parsed.arg),
                     }[parsed.type]));
                 }, false);
                 text.addEventListener("mouseout", function(){
